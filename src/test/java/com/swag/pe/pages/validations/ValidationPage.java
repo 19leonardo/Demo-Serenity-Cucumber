e3fd7443a11356b589266c03093c78503408a5c4
19leonardo <131973510+19leonardo@users.noreply.github.com>
2026-05-28 21:33:25 -0400

    Update ValidationPage.java

--- src/test/java/com/swag/pe/pages/validations/ValidationPage.java
+++ src/test/java/com/swag/pe/pages/validations/ValidationPage.java
@@ -5,7 +5,7 @@ import net.thucydides.core.pages.PageObject;
 import org.openqa.selenium.support.FindBy;
 
 public class ValidationPage extends PageObject {
-    @FindBy(css = "//span[@class='title']")
+    @FindBy(xpath = "//span[@class='title']")
     protected WebElementFacade lbl_product;
     @FindBy(css="h3[data-test='error']")
     protected WebElementFacade lbl_errorMessage;
